c960ca4a000c6429bd52ffc502f16d9af81ebcea
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-07-26 16:16:02 -0500

    feat: Add flask app for launching home template

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, Response, flash, redirect, url_for, abort
 
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def home():
+        return render_template('pages/home.html')
+
+    return app
+
+app = create_app()
+
+#----------------------------------------------------------------------------#
+# Launch.
+#----------------------------------------------------------------------------#
+
+if __name__ == '__main__':
+    app.run()
