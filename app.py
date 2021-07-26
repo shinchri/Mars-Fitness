b56a50343c8346c84b9eb3527779460fa1a3c4af
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-07-26 16:39:19 -0500

    feat: Add postgres database

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, request, Response, flash, redirect, url_for, abort
+from flask_sqlalchemy import SQLAlchemy
+from models import setup_db, User
 
 def create_app(test_config=None):
 
     app = Flask(__name__)
+    setup_db(app)
 
     @app.route('/')
     def home():
---
+++ models.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, String, Integer, DateTime, create_engine, Sequence
+from flask_sqlalchemy import SQLAlchemy
+import json
+import os
+
+db = SQLAlchemy()
+
+def setup_db(app):
+    app.config.from_object('config')
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+    return db
+
+class User(db.Model):
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
