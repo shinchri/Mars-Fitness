1a615255940cc2dff9cb835ef3483f6e5163e249
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-07-26 19:24:53 -0500

    feat: Made update to User class and created stripe user class

--- models.py
+++ models.py
@@ -1,7 +1,10 @@
-from sqlalchemy import Column, String, Integer, DateTime, create_engine, Sequence
+from sqlalchemy import Column, String, Integer, DateTime, ForeignKey, create_engine, Sequence
 from flask_sqlalchemy import SQLAlchemy
 import json
 import os
+import datetime
+
+from sqlalchemy.sql.functions import user
 
 db = SQLAlchemy()
 
@@ -14,14 +17,42 @@ def setup_db(app):
 
 class User(db.Model):
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
+    email = Column(String(128), nullable=False, unique=True)
+    password = Column(db.String(200), nullable=False)
+    created_on = Column(DateTime, default=datetime.datetime.now(), nullable=False)
 
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, email, password, created_on):
         self.first_name = first_name
         self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.created_on = created_on
     
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+class StripeCustomer(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    stripeCustomerId = Column(String(255), nullable=False)
+    stripeSubscriptionId = Column(String(255), nullable=False)
+
+    def __init__(self, user_id, stripeCustomerId, stripeSubscriptionId):
+        self.user_id = user_id
+        self.stripeCustomerId = stripeCustomerId
+        self.stripeSubscriptionId = stripeSubscriptionId
+
     def insert(self):
         db.session.add(self)
         db.session.commit()
