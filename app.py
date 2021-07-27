52a08ca8a72867e6a37f3c33659b2b8ee756e3ea
Chris Shin <17078706+shinchri@users.noreply.github.com>
2021-07-27 12:50:20 -0500

    feat: Add route for stripe checkout session

--- app.py
+++ app.py
@@ -26,6 +26,32 @@ def create_app(test_config=None):
         stripe_config = {"publicKey": stripe_keys["publishable_key"]}
         return jsonify(stripe_config)
 
+    @app.route("/create-checkout-session")
+    def create_checkout_session():
+        domain_url = "http://localhost:5000/"
+        stripe.api_key = stripe_keys["secret_key"]
+
+        try:
+            checkout_session = stripe.checkout.Session.create(
+                # You should get the user id here and pass it along as 'client_reference_id'
+                #
+                # this will alow you to associate the Stripe session with
+                # the user saved in your database
+                # 
+                # example: client_reference_id=user.id,
+                success_url = domain_url + "success?session_id={CHECKOUT_SESSION_ID}",
+                cancel_url = domain_url + "cancel",
+                payment_method_types=["card"],
+                mode="subscription",
+                line_items=[{
+                    "price": stripe_keys["price_id"],
+                    "quantity": 1,
+                }]
+            )
+            return jsonify({"sessionId": checkout_session["id"]})
+        except Exception as e:
+            return jsonify(error=str(e)), 403
+
     return app
 
 app = create_app()
